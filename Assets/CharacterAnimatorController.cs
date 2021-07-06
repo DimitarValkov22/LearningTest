a151259170222b17c19397b94b9f51ee0fd31d07
Dimitar Valkov <mitaka_1911@abv.bg>
2021-07-06 19:09:16 +0300

    Animations
    1.CharacterAnimator
    2.CharacterMovement

---
+++ Assets/CharacterAnimatorController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimatorController : MonoBehaviour
+{
+    private Animator anim;
+    private float speed = 2f;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+   
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("IsRunning", true);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetBool("IsGrounded", true);
+        }
+        else
+        {
+            anim.SetBool("IsGrounded", false);
+        }
+    }
+}
---
+++ Assets/MovementScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6f;
+    public float turnsmoothtime = 0.1f;
+    float turnSmoothVelocity;
+    public Transform cam;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetangle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetangle, ref turnSmoothVelocity, turnsmoothtime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetangle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
